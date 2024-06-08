b8201037972a276c1a1182619887915d1635f6cf
Zachary <zachahernandezz@gmail.com>
2024-06-08 14:50:19 -0600

    Cambios en User y UserXMLData

--- Proyecto1/src/main/java/module-info.java
+++ Proyecto1/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module ac.cr.ucr.proyecto1 {
     requires javafx.fxml;
     requires org.jdom2;
     requires java.mail;
+    requires java.logging;
 
 
     opens ucr.proyecto1 to javafx.fxml;
--- Proyecto1/src/main/java/ucr/proyecto1/domain/XMLData/UserXMLData.java
+++ Proyecto1/src/main/java/ucr/proyecto1/domain/XMLData/UserXMLData.java
@@ -9,60 +9,77 @@ import java.net.PasswordAuthentication;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.mail.*;
 import javax.mail.internet.*;
 
 public class UserXMLData {
 
     private CircularDoublyLinkedList usersList;
-    private final String from = "proyectoalgoritmos2024@gmail.com";
-    private final String pass = "uojf wcod qcau hcoy"; //Esto lo genere en google, contraseñas de aplicaciones
+    private final String emailFrom = "proyectoalgoritmos2024@gmail.com";
+    private final String passwordFrom = "uojf wcod qcau hcoy"; //Esto lo genere en google, contraseñas de aplicaciones
+    private String emailTo;
+    private String subject;
+    private String content;
 
-    public UserXMLData() {
+    private Properties props;
+    private Session session;
+    private MimeMessage mEmail;
+
+    public UserXMLData(){
+        props = new Properties();
         this.usersList = new CircularDoublyLinkedList();
     }
 
-    public void addUser(int id, String name, String email, String password) {
+    public void addUser(int id, String name, String email, String password) throws AddressException {
         User user = new User(id, name, email, password);
         usersList.add(user);
         emailNotification(email, id, password);
     }
 
-    private void emailNotification(String email, int id, String password) {
+
+    private void emailNotification(String email, int id, String password) throws AddressException {
         String hourDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        String register = "Successful Registration";
-        String sentence = "Date and hour of registration: " + hourDate + "\nUser: " + id+ "\nPassword: " + password;
 
-        //Configuración del correo (SMTP)
-        String host = "smtp.example.com"; // Cambia esto con tu servidor SMTP
+        emailTo = email;
+        subject = "Successful Registration";
+        content = "Date and hour of registration: " + hourDate + "\nUser: " + id+ "\nPassword: " + password;
 
-        Properties properties = System.getProperties();
-        properties.put("mail.smtp.host", host);
-        properties.put("mail.smtp.port", "587");
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
+        //Simple mail transfer protocol
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.setProperty("mail.smtp.starttls.enable", "true");
+        props.setProperty("mail.smtp.port", "587");
+        props.setProperty("mail.smtp.user", emailFrom);
+        props.setProperty("mail.smtp.ssl.protocols", "TLSv1.2");
+        props.setProperty("mail.smtp.auth", "true");
 
-        // Aquí utilizamos la variable de instancia `from` y `pass`
-        final String emailFrom = this.from;
-        final String emailPass = this.pass;
+        session = Session.getDefaultInstance(props);
 
-//        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
-//            protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-//                return new PasswordAuthentication(emailFrom, emailPass);
-//            }
-//        });
+        try {
+            mEmail = new MimeMessage(session);
+            mEmail.setFrom(new InternetAddress(emailFrom));
+            mEmail.setRecipient(Message.RecipientType.TO, new InternetAddress(emailTo));
+            mEmail.setSubject(subject);
+            mEmail.setText(content, "ISO-8859-1", "html");
+        }catch (AddressException ex){
+            Logger.getLogger(UserXMLData.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-//        try {
-//            MimeMessage message = new MimeMessage(session);
-//            message.setFrom(new InternetAddress(from));
-//            message.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
-//            message.setSubject(register);
-//            message.setText(sentence);
-//            Transport.send(message);
-//            System.out.println("Email sent successfully.");
-//        } catch (MessagingException mex) {
-//            mex.printStackTrace();
-//        }
+    private void sendEmail() throws MessagingException {
+        Transport transport = session.getTransport("smtp");
+        try {
+            transport.connect("smtp.gmail.com", emailFrom, passwordFrom);
+            transport.sendMessage(mEmail, mEmail.getAllRecipients());
+        } finally {
+            transport.close();
+        }
+
+        System.out.println("Email sent successfully.");
     }
 
     public boolean logIn(int id, String password) {
@@ -120,4 +137,6 @@ public class UserXMLData {
             e.printStackTrace();
         }
     }
+
 }
+

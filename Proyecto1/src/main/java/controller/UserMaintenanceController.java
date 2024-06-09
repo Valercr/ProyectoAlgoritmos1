34e9c1000be4bb97548ed2a95b6a666fd295b9cd
Valercr <valentina.rojasconsuegra@ucr.ac.cr>
2024-06-09 17:57:42 -0600

    agreg tableColum

--- Proyecto1/src/main/java/controller/UserMaintenanceController.java
+++ Proyecto1/src/main/java/controller/UserMaintenanceController.java
@@ -2,17 +2,22 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class UserMaintenanceController
 {
     @javafx.fxml.FXML
-    private Button addOnAction;
-    @javafx.fxml.FXML
-    private TextField searchTxtField;
-    @javafx.fxml.FXML
     private TableView tableView;
+    @javafx.fxml.FXML
+    private TableColumn roleTColumn;
+    @javafx.fxml.FXML
+    private TableColumn emailTColumn;
+    @javafx.fxml.FXML
+    private TableColumn idTColumn;
+    @javafx.fxml.FXML
+    private TableColumn nameTColumn;
 
     @javafx.fxml.FXML
     public void initialize() {
@@ -28,5 +33,9 @@ public class UserMaintenanceController
 
     @javafx.fxml.FXML
     public void deleteOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addOnAction(ActionEvent actionEvent) {
     }
 }
